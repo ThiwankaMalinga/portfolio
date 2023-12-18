00f6c43cc1d8c0a309a588fb7b0438a15d78372e
ThiwankaMalinga <thiwankamalinga0729@gmail.com>
2023-12-18 12:19:50 +0530

    feat: initial commit

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import '../Intro/intro.css';
+import bg from '../../assets/image.png';
+import btnImg from '../../assets/hireme.png'
+import { Link } from 'react-scroll'
 
 const Intro = () => {
   return (
-    <div>
-      
-    </div>
+    <section id="intro">
+        <div className='introContent'>
+            <span className='hello'>Hello</span>
+            <span className='introText'>I'm <span className='introName'>Thiwanka</span> <br /> Software Engineer</span>
+            <p className='introPara'>I am a skilled Software Engineer with experience in creating visually appealing and user friendly websites.</p>
+            <Link><button className='btn'><img src={btnImg} alt='Hire Me' />Hire Me</button></Link>
+        </div>
+        <img src={bg} alt='Profile' className='bg' />
+    </section>
   )
 }
 
