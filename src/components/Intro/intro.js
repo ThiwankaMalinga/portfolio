61495287ca0bbac0519f4dfceef43bbeac584284
ThiwankaMalinga <thiwankamalinga0729@gmail.com>
2023-12-19 16:21:04 +0530

    feat: initial commit

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -10,7 +10,7 @@ const Intro = () => {
         <div className='introContent'>
             <span className='hello'>Hello</span>
             <span className='introText'>I'm <span className='introName'>Thiwanka</span> <br /> Software Engineer</span>
-            <p className='introPara'>I am a skilled Software Engineer with experience in creating visually appealing and user friendly websites.</p>
+            <p className='introPara'>I am a skilled Software Engineer with experience in creating <br /> visually appealing and user friendly websites.</p>
             <Link><button className='btn'><img src={btnImg} alt='Hire Me' />Hire Me</button></Link>
         </div>
         <img src={bg} alt='Profile' className='bg' />
